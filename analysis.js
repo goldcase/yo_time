f3f2a88fe4b17bb6f92b8800bc74a69a64c161d0
Johnny Chang <johnny@johnnychang.com>
2016-11-05 20:50:34 -0700

    Added JS linting and build file.

--- analysis.js
+++ analysis.js
@@ -2,52 +2,52 @@
  * Analytics.js takes care of aggregation of tracker data.
  */
 function convertTimeToSeconds(time) {
-    return time/1000;
+    return time / 1000;
 }
 
 function getTotalTime() {
-    debugMessage("Called getTotalTime()");
+    debugMessage('Called getTotalTime()');
 
     storage_area.get(SITE_INTERVAL_MAP_KEY, function(items) {
         var interval_map = items[SITE_INTERVAL_MAP_KEY];
         var sum = 0.;
         for (var root_site_idx in interval_map) {
             var site_intervals = interval_map[root_site_idx];
-            var site_sum = 0.
+            var site_sum = 0.;
             for (var interval_idx in site_intervals) {
                 var target_interval = site_intervals[interval_idx];
                 site_sum += target_interval[1] - target_interval[0];
             }
-            debugMessage("The total amount of time tracked for site " + root_site_idx + " is " + convertTimeToSeconds(site_sum) + " seconds.");
+            debugMessage('The total amount of time tracked for site ' + root_site_idx + ' is ' + convertTimeToSeconds(site_sum) + ' seconds.');
             sum += site_sum;
         }
-        debugMessage("The total amount of time tracked by Yo is " + convertTimeToSeconds(sum) + " seconds.");
+        debugMessage('The total amount of time tracked by Yo is ' + convertTimeToSeconds(sum) + ' seconds.');
     });
 }
 
 function getTotalTimeForSite(root_site) {
-    debugMessage("Getting total time for site " + root_site);
+    debugMessage('Getting total time for site ' + root_site);
     storage_area.get(SITE_INTERVAL_MAP_KEY, function(items) {
-        debugMessage("Storage area retrieved.");
+        debugMessage('Storage area retrieved.');
         console.log(items);
         convertRootSiteToId(root_site, function(root_site_id, error_message) {
-            console.log("Root site converted to ID " + root_site_id);
+            console.log('Root site converted to ID ' + root_site_id);
             // Convert root_site string to ID and store the interval at the matching indices.
             if (root_site_id < 0) {
                 debugMessage(error_message);
             } else if (root_site_id < items[SITE_INTERVAL_MAP_KEY].length) {
                 // Sum up the total time interval at the target root site.
                 var sum = 0.;
-                debugMessage("Printing intervals.");
+                debugMessage('Printing intervals.');
                 var intervals = items[SITE_INTERVAL_MAP_KEY][root_site_id];
                 for (var interval_idx in intervals) {
                     var interval = intervals[interval_idx];
                     console.log(interval);
                     sum += interval[1] - interval[0];
                 }
-                debugMessage("The total amount of time tracked for site " + root_site + " is " + convertTimeToSeconds(sum) + " seconds.");
+                debugMessage('The total amount of time tracked for site ' + root_site + ' is ' + convertTimeToSeconds(sum) + ' seconds.');
             } else {
-                debugMessage("Root site ID is out of bounds of items[SITE_INTERVAL_MAP_KEY]. Error.");
+                debugMessage('Root site ID is out of bounds of items[SITE_INTERVAL_MAP_KEY]. Error.');
             }
         });
     });
--- tracker.js
+++ tracker.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /*
  * Tracker.js has the sole responsibility of tracking website usage.
@@ -9,40 +9,40 @@
 
 // Debug flag.
 var IS_DEBUG = true;
-var DEBUG_MESSAGE_PREFIX = "Debug log: ";
+var DEBUG_MESSAGE_PREFIX = 'Debug log: ';
 
 // Constants for names of items in storage.
-var START_TIME = "start_time";
-var END_TIME = "end_time";
-var ROOT_SITE_ID = "id";
-var CURRENT_URL = "current_url";
-var CURRENT_URL_START = "current_url_start";
-var SITE_INTERVAL_MAP_KEY = "site_to_interval";
+var START_TIME = 'start_time';
+var END_TIME = 'end_time';
+var ROOT_SITE_ID = 'id';
+var CURRENT_URL = 'current_url';
+var CURRENT_URL_START = 'current_url_start';
+var SITE_INTERVAL_MAP_KEY = 'site_to_interval';
 var THRESHOLD_INTERVAL = (new Date(5000)).getTime() - (new Date()).getTime();
-var ROOT_SITE_ENUM = "root_site_enum";
-var INVERSE_ROOT_SITE_MAP = "root_site_id_to_name";
-var ENUM_COUNTER = "root_site_enum_counter";
+var ROOT_SITE_ENUM = 'root_site_enum';
+var INVERSE_ROOT_SITE_MAP = 'root_site_id_to_name';
+var ENUM_COUNTER = 'root_site_enum_counter';
 var ENUM_COUNTER_START = 0;
 
 // Sites you want to induce latency in.
-var BLACKLIST_KEY = "blacklist";
+var BLACKLIST_KEY = 'blacklist';
 
 // Sites you don't want to track.
-var WHITELIST_KEY = "whitelist";
+var WHITELIST_KEY = 'whitelist';
 
-var ALL_GOOD = "";
-var ROOT_SITE_ID_STRING = "root site ID string";
-var ROOT_SITE_STRING = "root site string";
+var ALL_GOOD = '';
+var ROOT_SITE_ID_STRING = 'root site ID string';
+var ROOT_SITE_STRING = 'root site string';
 
 var current_tab_info = {};
 var storage_area = chrome.storage.local;
 
 function doesNotExist(requested_target) {
-    return "The requested " + requested_target + " does not exist.";
+    return 'The requested ' + requested_target + ' does not exist.';
 }
 
 function formatErrorString(prefix, target_string) {
-    return prefix + " of " + target_string;
+    return prefix + ' of ' + target_string;
 }
 
 // Assumes root site enum has already been initialized.
@@ -67,7 +67,7 @@ function convertRootSiteToId(root_site, callback) {
 // function callback(string root_site_string, string error_message) { ... }
 function convertIdToRootSiteString(root_site_id, callback) {
     storage_area.get(INVERSE_ROOT_SITE_MAP, function(items) {
-        var root_site_string = "";
+        var root_site_string = '';
         var error_message = doesNotExist(formatErrorString(ROOT_SITE_STRING, root_site_id));
 
         if (items[INVERSE_ROOT_SITE_MAP].length > root_site_id) {
@@ -81,11 +81,11 @@ function convertIdToRootSiteString(root_site_id, callback) {
 // Check if a root_site is already being tracked. If not, insert the root site
 // into the enum and update the corresponding enum-value based data structures.
 function insertRootSite(root_site, callback) {
-    debugMessage("Attempting to insert a root site into storage.");
+    debugMessage('Attempting to insert a root site into storage.');
     storage_area.get([ROOT_SITE_ENUM, INVERSE_ROOT_SITE_MAP, ENUM_COUNTER, SITE_INTERVAL_MAP_KEY], function(items) {
         if (!(root_site in items[ROOT_SITE_ENUM])) {
-            debugMessage("Pushing " + root_site + " onto our maps and enums. " +
-                "Printing Site interval map key:");
+            debugMessage('Pushing ' + root_site + ' onto our maps and enums. ' +
+                'Printing Site interval map key:');
 
             items[INVERSE_ROOT_SITE_MAP].push(root_site);
             items[ROOT_SITE_ENUM][root_site] = items[ENUM_COUNTER];
@@ -93,7 +93,7 @@ function insertRootSite(root_site, callback) {
             items[SITE_INTERVAL_MAP_KEY].push([]);
             console.log(items[SITE_INTERVAL_MAP_KEY]);
             items[ENUM_COUNTER]++;
-            console.log("num items in site_interval_map: " + items[SITE_INTERVAL_MAP_KEY].length + " should match " + items[ENUM_COUNTER]);
+            console.log('num items in site_interval_map: ' + items[SITE_INTERVAL_MAP_KEY].length + ' should match ' + items[ENUM_COUNTER]);
 
             storage_area.set(items, function() {
                 callback();
@@ -107,14 +107,14 @@ function insertRootSite(root_site, callback) {
 function initializeEnum() {
     storage_area.get([ROOT_SITE_ENUM, ENUM_COUNTER, INVERSE_ROOT_SITE_MAP, SITE_INTERVAL_MAP_KEY], function(items) {
         if (!(ROOT_SITE_ENUM in items)) {
-            debugMessage("Initializing root site enum, the counter, root site map, and site interval map key.");
+            debugMessage('Initializing root site enum, the counter, root site map, and site interval map key.');
             items[ROOT_SITE_ENUM] = {};
             items[INVERSE_ROOT_SITE_MAP] = [];
             items[ENUM_COUNTER] = ENUM_COUNTER_START;
             items[SITE_INTERVAL_MAP_KEY] = [];
         }
         storage_area.set(items, function() {
-            debugMessage("Initialized storage area for root site enums!");
+            debugMessage('Initialized storage area for root site enums!');
         });
     });
 }
@@ -165,15 +165,15 @@ function createInterval(start_time, end_time) {
 // Records a tracking interval for some site.
 // TODO(johnnychang): Refactor this to be smaller.
 function setSite(root_site, start_date, end_date, callback) {
-    debugMessage("Called setSite.");
+    debugMessage('Called setSite.');
     console.log(start_date);
     console.log(end_date);
     var start_time = start_date.getTime();
     var end_time = end_date.getTime();
-    debugMessage("Adding " + start_time + ", " + end_time + " to " + root_site + "!");
+    debugMessage('Adding ' + start_time + ', ' + end_time + ' to ' + root_site + '!');
 
     if (end_time - start_time < THRESHOLD_INTERVAL) {
-        debugMessage("Not adding to storage if under threshold of " + THRESHOLD_INTERVAL);
+        debugMessage('Not adding to storage if under threshold of ' + THRESHOLD_INTERVAL);
         return;
     }
 
@@ -185,12 +185,12 @@ function setSite(root_site, start_date, end_date, callback) {
             } else {
                 // TODO(johnnychang): Check if this is ever out of sync with the interned root_site ids.
                 if (root_site_id < items[SITE_INTERVAL_MAP_KEY].length &&
-                    typeof start_time !== "undefined" &&
-                    typeof end_time !== "undefined") {
+                    typeof start_time !== 'undefined' &&
+                    typeof end_time !== 'undefined') {
                     var interval = createInterval(start_time, end_time);
                     if (interval.length == 2) {
                         console.log(items[SITE_INTERVAL_MAP_KEY]);
-                        console.log("Root site id: " + root_site_id);
+                        console.log('Root site id: ' + root_site_id);
                         console.log(interval);
                         items[SITE_INTERVAL_MAP_KEY][root_site_id].push(interval);
                     }
@@ -198,7 +198,7 @@ function setSite(root_site, start_date, end_date, callback) {
 
                 // Update storage.
                 storage_area.set(items, function() {
-                    debugMessage("Successfully set storage!");
+                    debugMessage('Successfully set storage!');
                     callback();
                 });
             }
@@ -208,7 +208,7 @@ function setSite(root_site, start_date, end_date, callback) {
 
 // Locally records the start of tracking.
 function startTracking(root_site, start_time) {
-    debugMessage("Called startTracking on " + root_site + "!");
+    debugMessage('Called startTracking on ' + root_site + '!');
     current_tab_info[CURRENT_URL] = root_site;
     current_tab_info[CURRENT_URL_START] = start_time;
 }
@@ -219,15 +219,15 @@ function clearCurrentTabInfo() {
 
 // Finishes tracking of the url stored in current_tab_info.
 function finishTracking(callback) {
-    debugMessage("Called finishTracking!");
+    debugMessage('Called finishTracking!');
 
     console.log(current_tab_info);
 
     // Check that there is a URL defined in current_tab_info.
     if (CURRENT_URL_START in current_tab_info) {
-        debugMessage("URL defined; proceeding to record interval.");
+        debugMessage('URL defined; proceeding to record interval.');
         var current_time = new Date();
-        if (typeof current_tab_info[CURRENT_URL_START] !== "undefined" &&
+        if (typeof current_tab_info[CURRENT_URL_START] !== 'undefined' &&
                 current_tab_info[CURRENT_URL_START] instanceof Date) {
             // Record interval for current site.
             setSite(current_tab_info[CURRENT_URL], current_tab_info[CURRENT_URL_START], current_time,
@@ -242,7 +242,7 @@ function finishTracking(callback) {
 }
 
 function debugMessage(msg) {
-    if (IS_DEBUG && typeof msg === "string") {
+    if (IS_DEBUG && typeof msg === 'string') {
         console.log(DEBUG_MESSAGE_PREFIX + msg);
     }
 }
@@ -264,9 +264,9 @@ function parseUrlForRootSite(url_string) {
 
 function trackTime(url_string) {
     finishTracking(function() {
-        debugMessage("Called trackTime callback. Attempting to reset startTracking.");
+        debugMessage('Called trackTime callback. Attempting to reset startTracking.');
         var root_site = parseUrlForRootSite(url_string);
-        debugMessage("Root_site: " + root_site);
+        debugMessage('Root_site: ' + root_site);
         var start_time = new Date();
         insertRootSite(root_site, function() {
             startTracking(root_site, start_time);
@@ -277,11 +277,11 @@ function trackTime(url_string) {
 function checkForActiveTab() {
     chrome.tabs.query(createActiveQuery(), function(tab_array) {
         if (tab_array.length > 1) {
-            debugMessage("Tab array length should not be greater than 1.");
+            debugMessage('Tab array length should not be greater than 1.');
             return;
         } else if (tab_array.length > 0) {
             var active_tab = tab_array[0];
-            if (active_tab.status == "complete") {
+            if (active_tab.status == 'complete') {
                 trackTime(active_tab.url);
             }
         }
@@ -289,7 +289,7 @@ function checkForActiveTab() {
 }
 
 function getActiveTab(tabId, changeInfo, tab) {
-    if (typeof tab !== "undefined" && tab.status == "complete") {
+    if (typeof tab !== 'undefined' && tab.status == 'complete') {
         checkForActiveTab();
     }
 }
@@ -306,7 +306,7 @@ function getCurrentState() {
 
 function clearStorageArea() {
     storage_area.clear(function() {
-        debugMessage("Cleared storage area!");
+        debugMessage('Cleared storage area!');
     });
 }
 
@@ -316,4 +316,4 @@ chrome.tabs.onUpdated.addListener(getActiveTab);
 chrome.tabs.onCreated.addListener(getActiveTab);
 chrome.tabs.onActivated.addListener(trackActiveChange);
 
-alert("Loaded Yo Time!");
+alert('Loaded Yo Time!');
