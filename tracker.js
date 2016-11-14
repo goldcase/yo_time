fc2859b15a2711aec4192fb1abb910ba4efb84ee
Johnny Chang <johnny@johnnychang.com>
2016-11-13 23:02:08 -0600

    Linted code.

--- tracker.js
+++ tracker.js
@@ -15,18 +15,18 @@ var IS_ERROR = true;
 var DEBUG_LOG_PREFIX = 'Debug log: ';
 var ERROR_LOG_PREFIX = 'Error: ';
 
-var ALL_GOOD            = '';
+var ALL_GOOD = '';
 var ROOT_SITE_ID_STRING = 'root site ID string';
-var ROOT_SITE_STRING    = 'root site string';
+var ROOT_SITE_STRING = 'root site string';
 
 var INTERVAL_START_IDX = 0;
-var INTERVAL_END_IDX   = 1;
+var INTERVAL_END_IDX = 1;
 
 var current_tab_info = {};
-var storage_area     = chrome.storage.local;
+var storage_area = chrome.storage.local;
 
 // Class constants.
-var INTERVAL_UNFINISHED = "The interval is not currently finished with construction.";
+var INTERVAL_UNFINISHED = 'The interval is not currently finished with construction.';
 
 var website_container = null;
 var analyzer = null;
@@ -37,7 +37,7 @@ class Interval {
      */
     constructor(interval_start, interval_end) {
         if (!(interval_start instanceof Date && interval_end instanceof Date)) {
-            errorLog("Error: Attempted to construct interval object with non-Date parameters.");
+            errorLog('Error: Attempted to construct interval object with non-Date parameters.');
         }
 
         this.start = !!interval_start && interval_start instanceof Date ? interval_start.getTime() : (new Date()).getTime();
@@ -55,20 +55,20 @@ class Interval {
         if (this.isFinished()) {
             difference = this.end - this.start;
         } else {
-            errorMessage = "Unable to return a difference if the end is not set.";
+            errorMessage = 'Unable to return a difference if the end is not set.';
         }
 
         return difference;
     }
 
     setStart(interval_start) {
-        this.start = interval_start.constructor.name === "Date" ? interval_start.getTime() : interval_start;
+        this.start = interval_start.constructor.name === 'Date' ? interval_start.getTime() : interval_start;
     }
 
     setEnd(interval_end) {
-        this.end = interval_end.constructor.name === "Date" ? interval_end.getTime() : interval_end;
+        this.end = interval_end.constructor.name === 'Date' ? interval_end.getTime() : interval_end;
     }
-}
+};
 
 var THRESHOLD_INTERVAL_LENGTH = (new Interval(new Date(0), new Date(20))).getLength();
 
@@ -90,20 +90,20 @@ class TrackedWebsite {
      * Takes in an already-created Interval object.
      */
     addInterval(interval, errorMessage, callback) {
-        debugLog("Adding interval to current tracked website: " + this.site_name);
+        debugLog('Adding interval to current tracked website: ' + this.site_name);
 
         // Check that interval is finished being constructed.
         if (!interval.isFinished()) {
-            errorLog("Interval appears unfinished.");
+            errorLog('Interval appears unfinished.');
             errorMessage = INTERVAL_UNFINISHED;
             return;
         }
 
         console.log(this.usage_intervals);
-        debugLog("Interval length before push: " + this.usage_intervals.length);
+        debugLog('Interval length before push: ' + this.usage_intervals.length);
         this.usage_intervals.push(interval);
-        debugLog("Interval length after push: " + this.usage_intervals.length);
-        console.log("Pushed interval onto THIS usage interval list.");
+        debugLog('Interval length after push: ' + this.usage_intervals.length);
+        console.log('Pushed interval onto THIS usage interval list.');
         console.log(interval);
         console.log(this);
         callback();
@@ -114,7 +114,7 @@ class TrackedWebsite {
         var average_interval_length = 0;
 
         if (this.usage_intervals.length === 0) {
-            errorMessage = "There are no intervals currently available for this website.";
+            errorMessage = 'There are no intervals currently available for this website.';
             return average_interval_length;
         }
 
@@ -140,19 +140,20 @@ class TrackedWebsite {
 
     createExtremaObject(min_interval, max_interval) {
         return {
-            "max": max_interval,
-            "min": min_interval
+            'max': max_interval,
+            'min': min_interval
         };
     }
 
     getExtremaBetween(start_time, end_time, errorMessage) {
         var zero_interval = new Interval(new Date(), new Date());
-        var min_interval = zero_interval;
+        var max_interval_limit = new Interval(new Date(0), new Date());
+        var min_interval = max_interval_limit;
         var max_interval = zero_interval;
 
         if (this.usage_intervals.length === 0) {
-            errorMessage = "There are no intervals currently available for this website.";
-            return this.createExtremaObject(max_interval, min_interval);
+            errorMessage = 'There are no intervals currently available for this website.';
+            return this.createExtremaObject(min_interval, max_interval);
         }
 
         if (!start_time) {
@@ -179,40 +180,42 @@ class TrackedWebsite {
     }
 
     serialize() {
-        debugLog("Called serialize on:");
+        debugLog('Called serialize on:');
         console.log(this);
         var serialized_value = JSON.stringify(this);
-        debugLog("Serialized_value: " + serialized_value);
+        debugLog('Serialized_value: ' + serialized_value);
         return serialized_value;
     }
 
     static deserialize(candidate_tracked_website) {
-        debugLog("Deserializing: " + candidate_tracked_website);
+        debugLog('Deserializing: ' + candidate_tracked_website);
         var parsed_candidate = JSON.parse(candidate_tracked_website);
         console.log(parsed_candidate);
-        if (!("site_name" in parsed_candidate)) {
-            errorLog("Site name not found in parse target for a TrackedWebsite object.");
+        if (!('site_name' in parsed_candidate)) {
+            errorLog('Site name not found in parse target for a TrackedWebsite object.');
         }
-        var tracked_website = new TrackedWebsite(parsed_candidate["site_name"]);
+        var tracked_website = new TrackedWebsite(parsed_candidate['site_name']);
         for (var candidate_property in parsed_candidate) {
-            if (candidate_property === "usage_intervals") {
+            if (candidate_property === 'usage_intervals') {
                 var target_values = parsed_candidate[candidate_property];
                 for (var interval_idx in target_values) {
                     // TODO(johnnychang): Check that the candidate parsed object is properly formed.
-                    var start_time = target_values[interval_idx]["start"];
-                    var end_time = target_values[interval_idx]["end"];
+                    var start_time = target_values[interval_idx]['start'];
+                    var end_time = target_values[interval_idx]['end'];
                     var target_interval = new Interval(start_time, end_time);
 
+                    debugLog(target_interval.getLength());
+
                     // TODO(johnnychang): Handle error message here.
-                    tracked_website.addInterval(target_interval, "", function() {
-                        debugLog("Added interval to unpickled TrackedWebsite.");
+                    tracked_website.addInterval(target_interval, '', function() {
+                        debugLog('Added interval to unpickled TrackedWebsite.');
                     });
                 }
             }
         }
         return tracked_website;
     }
-}
+};
 
 /*
  * Abstracts away Chrome storage area management.
@@ -224,7 +227,7 @@ class WebsiteContainer {
     }
 
     addIntervalToWebsite(site_name, interval, errorMessage, callback) {
-        debugLog("WebsiteContainer adding interval.");
+        debugLog('WebsiteContainer adding interval.');
 
         // Check if interval is done being constructed.
         if (!interval.isFinished()) {
@@ -236,7 +239,7 @@ class WebsiteContainer {
         var that = this;
         this.getWebsite(site_name, function(tracked_website) {
             var addIntervalAndUpdate = function(target_site, callback) {
-                debugLog("Attempted to add interval.");
+                debugLog('Attempted to add interval.');
                 // Given a tracked website, add an interval to it and update the storage area.
                 target_site.addInterval(interval, errorMessage, function() {
                     that.updateItems(site_name, target_site, callback);
@@ -251,14 +254,14 @@ class WebsiteContainer {
                 });
             } else {
                 addIntervalAndUpdate(tracked_website, function() {
-                    debugLog("Added interval and updated items.");
+                    debugLog('Added interval and updated items.');
                 });
             }
         });
     }
 
     addWebsite(site_name, callback) {
-        debugLog("Called addWebsite on " + site_name);
+        debugLog('Called addWebsite on ' + site_name);
         var that = this;
         // TODO(johnnychang): Check chrome.runtime.lastError for all get calls.
         this.storage_area.get(null, function(items) {
@@ -267,20 +270,20 @@ class WebsiteContainer {
             }
             var tracked_website = new TrackedWebsite(site_name);
             that.updateItems(site_name, tracked_website, function() {
-                debugLog("Attempted to update items.");
+                debugLog('Attempted to update items.');
                 callback(tracked_website);
             });
         });
     }
 
     getWebsite(site_name, callback) {
-        debugLog("Called getWebsite on " + site_name);
+        debugLog('Called getWebsite on ' + site_name);
         storage_area.get(site_name, function(items) {
-            var errorMessage = "";
+            var errorMessage = '';
             // If site is not already tracked, create an entry in the storage area dictionary.
             if (!(site_name in items)) {
-                items[site_name] = "";
-                errorMessage = "The current site, " + site_name + ", hasn't been tracked yet.";
+                items[site_name] = '';
+                errorMessage = 'The current site, ' + site_name + ", hasn't been tracked yet.";
                 callback(null, errorMessage);
                 return;
             }
@@ -292,20 +295,20 @@ class WebsiteContainer {
     }
 
     updateItems(site_name, tracked_website, callback) {
-        debugLog("Called updateItems on site_name");
+        debugLog('Called updateItems on site_name');
         // Responsible for serialization, deserialization.
         var items = {};
-        var serialized_value = "";
+        var serialized_value = '';
         serialized_value = tracked_website.serialize();
         items[site_name] = serialized_value;
 
         this.storage_area.set(items, function() {
             // TODO(johnnychang): Check that storage area can return some sort of success state.
-            debugLog("Attempted storage area update.");
+            debugLog('Attempted storage area update.');
             callback();
         });
     }
-}
+};
 
 /*
  * Performs analysis operations on tracked websites given a website container.
@@ -316,8 +319,10 @@ class Analyzer {
     }
 
     getTrackedWebsite(site_name, callback) {
+        debugLog('Called getTrackedWebsite.');
+
         this.website_container.getWebsite(site_name, function(tracked_website, errorMessage) {
-            if (!errorMessage) {
+            if (!!errorMessage) {
                 callback(null, errorMessage);
                 return;
             }
@@ -327,8 +332,10 @@ class Analyzer {
     }
 
     getExtremaBetween(start_time, end_time, site_name, callback) {
+        debugLog('Called getExtremaBetween.');
+
         this.getTrackedWebsite(site_name, function(tracked_website, errorMessage) {
-            if (!errorMessage) {
+            if (!!errorMessage) {
                 callback(null, errorMessage);
                 return;
             }
@@ -339,8 +346,10 @@ class Analyzer {
     }
 
     getMaxBetween(start_time, end_time, site_name, callback) {
+        debugLog('Called getMaxBetween.');
+
         this.getExtremaBetween(start_time, end_time, site_name, function(extrema, errorMessage) {
-            if (!errorMessage) {
+            if (!!errorMessage) {
                 callback(null, errorMessage);
                 return;
             }
@@ -350,7 +359,7 @@ class Analyzer {
 
     getMinBetween(start_time, end_time, site_name, callback) {
         this.getExtremaBetween(start_time, end_time, site_name, function(extrema, errorMessage) {
-            if (!errorMessage) {
+            if (!!errorMessage) {
                 callback(null, errorMessage);
                 return;
             }
@@ -360,7 +369,7 @@ class Analyzer {
 
     getAverageBetween(start_time, end_time, site_name, callback) {
         this.getTrackedWebsite(site_name, function(tracked_website, errorMessage) {
-            console.log("Tracked website:");
+            console.log('Tracked website:');
             console.log(tracked_website);
             if (!tracked_website) {
                 callback(null, errorMessage);
@@ -371,14 +380,14 @@ class Analyzer {
             callback(average_interval_length, errorMessage);
         });
     }
-}
+};
 
 var current_interval = new Interval(null, null);
-var current_site_name = "";
+var current_site_name = '';
 
 function resetTrackingVariables() {
     current_interval = new Interval(null, null);
-    current_site_name = "";
+    current_site_name = '';
 }
 
 // Locally records the start of tracking.
@@ -391,23 +400,23 @@ function startTracking(root_site) {
 // Finishes tracking of the url stored in current_tab_info.
 function finishTracking(current_site, site_interval, errorMessage, callback) {
     debugLog('Called finishTracking! on ' + current_site);
-    console.log("Current site interval: " + site_interval);
+    console.log('Current site interval: ' + site_interval);
 
     if (!current_site) {
-        debugLog("Empty site.");
-        errorMessage = "Empty site.";
+        debugLog('Empty site.');
+        errorMessage = 'Empty site.';
         callback();
         return;
     }
 
     if (site_interval.getLength() < THRESHOLD_INTERVAL_LENGTH) {
-        debugLog("Below threshold.");
+        debugLog('Below threshold.');
         errorMessage = "Current interval length hasn't crossed our threshold.";
         callback();
         return;
     }
 
-    debugLog("Proceeding to add current interval to website.");
+    debugLog('Proceeding to add current interval to website.');
     // Record interval for current site.
     website_container.addIntervalToWebsite(current_site, site_interval, errorMessage, callback);
 }
@@ -428,12 +437,12 @@ function parseUrlForRootSite(url_string) {
 }
 
 function trackTime(url_string) {
-    debugLog("Tracking time for " + url_string);
+    debugLog('Tracking time for ' + url_string);
     // Calls finishTracking on current site with current interval.
-    var errorMessage = "";
+    var errorMessage = '';
     var last_site = current_site_name;
     current_site_name = parseUrlForRootSite(url_string);
-    debugLog("Last site: " + last_site + ", current_site: " + current_site_name);
+    debugLog('Last site: ' + last_site + ', current_site: ' + current_site_name);
     current_interval.setEnd(new Date());
 
     finishTracking(last_site, current_interval, errorMessage, function() {
@@ -444,11 +453,11 @@ function trackTime(url_string) {
 }
 
 function checkForActiveTab(errorMessage) {
-    debugLog("called checkForActiveTab.");
+    debugLog('called checkForActiveTab.');
 
     chrome.tabs.query(createActiveQuery(), function(tab_array) {
         if (tab_array.length > 1) {
-            errorMessage = "Tab array length should not be greater than 1.";
+            errorMessage = 'Tab array length should not be greater than 1.';
         } else if (tab_array.length == 1) {
             var active_tab = tab_array[0];
             if (active_tab.status == 'complete') {
@@ -461,23 +470,23 @@ function checkForActiveTab(errorMessage) {
                 */
             }
         } else {
-            errorMessage = "Tab array length is incorrect.";
+            errorMessage = 'Tab array length is incorrect.';
         }
     });
 }
 
 function getActiveTab(tabId, changeInfo, tab) {
-    debugLog("Called getActiveTab.");
+    debugLog('Called getActiveTab.');
     console.log(changeInfo);
     if (typeof tab !== 'undefined' && tab.status == 'complete' && typeof tab.url !== 'undefined') {
-        var errorMessage = "";
+        var errorMessage = '';
         checkForActiveTab(errorMessage);
         errorLog(errorMessage);
     }
 }
 
 function trackActiveChange(activeInfo) {
-    debugLog("Called trackActiveChange.");
+    debugLog('Called trackActiveChange.');
     checkForActiveTab();
 }
 
@@ -494,13 +503,13 @@ function clearStorageArea() {
 }
 
 function debugLog(debug_message) {
-    if (typeof debug_message === "string") {
+    if (typeof debug_message === 'string') {
         console.log(DEBUG_LOG_PREFIX + debug_message);
     }
 }
 
 function errorLog(error_message) {
-    if (typeof debug_message === "string") {
+    if (typeof debug_message === 'string') {
         console.log(ERROR_LOG_PREFIX + error_message);
     }
 }
@@ -513,12 +522,12 @@ function initialize() {
 initialize();
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    debugLog("Received update event.");
+    debugLog('Received update event.');
     getActiveTab(tabId, changeInfo, tab);
 });
 
 chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
-    debugLog("Received created event.");
+    debugLog('Received created event.');
     getActiveTab(tabId, changeInfo, tab);
 });
 chrome.tabs.onActivated.addListener(trackActiveChange);
